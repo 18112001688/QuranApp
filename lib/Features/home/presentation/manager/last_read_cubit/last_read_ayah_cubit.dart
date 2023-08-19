878642b1131674829920807d4bb3e953aade72b6
Moustafa Mahmoud <moustafa167@gmail.com>
2023-08-19 15:52:23 +0300

    last read ayah created and trigered

--- lib/Features/home/presentation/manager/last_read_cubit/last_read_ayah_cubit.dart
+++ lib/Features/home/presentation/manager/last_read_cubit/last_read_ayah_cubit.dart
@@ -7,10 +7,17 @@ class LastReadAyahCubit extends Cubit<LastReadAyahState> {
   LastReadAyahCubit()
       : super(LastReadAyahSuccess(surahName: '', ayahNumber: 0));
 
+  Future<void> getLastReadAyah() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    String surahName = prefs.getString("LastReadSurahName") ?? '';
+    int ayahNumber = prefs.getInt("LastReadSurahNumber") ?? 0;
+    emit(LastReadAyahSuccess(surahName: surahName, ayahNumber: ayahNumber));
+  }
+
   Future<void> setLastReadAyah(String surahName, int ayahNumber) async {
-  SharedPreferences prefs = await SharedPreferences.getInstance();
-  await prefs.setString("LastReadSurahName", surahName);
-  await prefs.setInt("LastReadSurahNumber", ayahNumber);
-  emit(LastReadAyahSuccess(surahName: surahName, ayahNumber: ayahNumber));
-}
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.setString("LastReadSurahName", surahName);
+    await prefs.setInt("LastReadSurahNumber", ayahNumber);
+    emit(LastReadAyahSuccess(surahName: surahName, ayahNumber: ayahNumber));
+  }
 }
